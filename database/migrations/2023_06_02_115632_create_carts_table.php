47e57ede40123d94428a80a0214ed76c6881d6c7
accentout <123963577+accentout@users.noreply.github.com>
2023-06-02 20:07:48 +0500

    fix: migration(cart table)

--- database/migrations/2023_06_02_115632_create_carts_table.php
+++ database/migrations/2023_06_02_115632_create_carts_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreignId('product_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('user_id');
+            $table->foreignId('product_id');
             $table->timestamps();
         });
     }
